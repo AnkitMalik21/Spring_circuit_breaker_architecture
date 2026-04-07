57c329e8ce6709f165f9316d1c89312d7e326c81
Ankit kumar malik <ankit2102malik@gmail.com>
2026-04-07 12:37:32 +0530

    added file in config repo

---
+++ HotelService/src/main/java/com/icwd/hotel/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.icwd.hotel.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
+        security.authorizeHttpRequests(authorize -> authorize
+                .anyRequest().authenticated()
+        ).oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}));
+        
+        return security.build();
+    }
+}
